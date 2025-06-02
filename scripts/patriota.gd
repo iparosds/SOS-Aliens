90b5f9f49a7b02e41b53ab916ea2a4b585782f35
iBerserker89 <lucianof.barros89@gmail.com>
2025-06-02 17:18:53 -0300

    Criando explosão.

--- scripts/patriota.gd
+++ scripts/patriota.gd
@@ -3,7 +3,8 @@ class_name Patriota extends CharacterBody2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 @onready var animation_player: AnimatedSprite2D = $AnimationPlayer
 @onready var click_area: Area2D = $ClickArea
-@onready var kill_timer: Timer = $KillTimer
+
+@export var deathParticle: PackedScene
 
 # Velocidade de movimentação da barata (pode ser configurada no editor)
 @export var speed: float = 100.0
@@ -18,12 +19,11 @@ signal clicked
 func _ready():
 	navigation_agent_2d.target_position = saida
 	click_area.input_event.connect(_on_click)
-	kill_timer.timeout.connect(queue_free)
 	
 func _on_click(viewport:Node, event:InputEvent, shape_idx:int):
 	if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.pressed:
 		emit_signal("clicked")
-		kill_timer.start()
+		kill()
 	
 func _physics_process(delta):
 	# Se o agente já chegou ao destino, remove a barata da cena
@@ -40,3 +40,32 @@ func _physics_process(delta):
 	
 	# Move a barata de acordo com a física do Godot
 	move_and_slide()
+
+## Freeze e delay antes de explodir.
+#func kill():
+	#animation_player.stop()
+	#click_area.set_deferred("disabled", true)  # evita múltiplos cliques
+	#set_physics_process(false)
+#
+	#await get_tree().create_timer(0.4).timeout
+#
+	#var _particle = deathParticle.instantiate()
+	#_particle.global_position = global_position
+	#_particle.rotation = direction.angle()
+	#_particle.emitting = true
+	#get_tree().current_scene.add_child(_particle)
+#
+	#queue_free()
+
+
+func kill():
+	var _particle = deathParticle.instantiate()
+	_particle.global_position = global_position
+	_particle.rotation = direction.angle()
+	_particle.emitting = true
+	get_tree().current_scene.add_child(_particle)
+
+	queue_free()
+
+	await get_tree().create_timer(0.4).timeout
+	_particle.queue_free()
